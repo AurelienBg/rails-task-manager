7fce3c073fbc8358fa13356da6a0286fc856cadc
Aurelien Burget <aurelion@gmail.com>
2017-02-07 14:26:46 +0100

    adding tasks actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,20 +4,39 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    # no need for app/views/task/create.html.erb
+    redirect_to tasks_path
   end
 
   def new
+    @task = Task.new()
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # no need for app/views/task/create.html.erb
+    redirect_to tasks_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.delete
   end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :priority)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 
-
 #     tasks GET    /tasks(.:format)          tasks#index
 #           POST   /tasks(.:format)          tasks#create
 #  new_task GET    /tasks/new(.:format)      tasks#new
